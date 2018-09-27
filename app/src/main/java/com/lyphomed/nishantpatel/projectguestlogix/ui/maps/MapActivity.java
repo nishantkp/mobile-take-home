6005a1adcb3d9981753510d87785698451c10925
Nishant Patel <nishantuwindsor@gmail.com>
2018-09-26 20:18:14 -0400

    Create Map activity

---
+++ app/src/main/java/com/lyphomed/nishantpatel/projectguestlogix/ui/maps/MapActivity.java
@@ -0,0 +1,15 @@
+package com.lyphomed.nishantpatel.projectguestlogix.ui.maps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lyphomed.nishantpatel.projectguestlogix.R;
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
