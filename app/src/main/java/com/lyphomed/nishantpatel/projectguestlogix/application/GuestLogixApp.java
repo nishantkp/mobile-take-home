5f32e52a627800b6b59361bed3745969541a70cc
Nishant Patel <nishantuwindsor@gmail.com>
2018-09-27 18:39:18 -0400

    Move unnecessary imports from Application class

--- app/src/main/java/com/lyphomed/nishantpatel/projectguestlogix/application/GuestLogixApp.java
+++ app/src/main/java/com/lyphomed/nishantpatel/projectguestlogix/application/GuestLogixApp.java
@@ -1,21 +1,9 @@
 package com.lyphomed.nishantpatel.projectguestlogix.application;
 
 import android.app.Application;
-import android.util.Log;
 
-import com.lyphomed.nishantpatel.projectguestlogix.data.local.callbacks.OnTaskCompletion;
-import com.lyphomed.nishantpatel.projectguestlogix.data.local.database.model.Routes;
 import com.lyphomed.nishantpatel.projectguestlogix.data.local.database.schema.AirlinesDatabase;
 import com.lyphomed.nishantpatel.projectguestlogix.data.manager.DataManager;
-import com.lyphomed.nishantpatel.projectguestlogix.ui.model.ViaRoute;
-import com.lyphomed.nishantpatel.projectguestlogix.utils.TableDataCreation;
-
-
-import java.util.List;
-
-import io.reactivex.Flowable;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * Application class
